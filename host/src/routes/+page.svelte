5e56d84d192af970967e4d5c0b82953e0601ade1
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-02 19:59:08 +0300

    Setting up files

--- host/src/routes/+page.svelte
+++ host/src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+
+	let Header;
+	if (browser)
+		(async () => {
+			// @ts-ignore
+			const component = await import("remote/header");
+			new component.default({ target: Header, props: { type: "host" } });
+		})();
+</script>
+
+<header bind:this={Header}></header>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ remote/src/lib/components/Header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	interface Props {
+		[key: string]: any;
+		type: "remote" | "host";
+	}
+
+	let { type, ...props }: Props = $props();
+</script>
+
+<header {...props}>
+	<p>This is my {type} header</p>
+</header>
+
+<style>
+	header {
+		background-color: #f00;
+		color: #fff;
+		padding: 1rem;
+	}
+
+	p {
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+</style>
